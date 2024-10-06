4b84b3d03278afa871b2b0ed3dfa3ed6f26d234f
Andrexus23 <andrewsamsunguser@gmail.com>
2024-10-06 19:43:24 +0300

    update

--- fastapi-app/app/db.py
+++ fastapi-app/app/db.py
@@ -18,7 +18,8 @@ class Timestamp(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     timestamp: Mapped[int] = mapped_column(DateTime)
 
-engine: Engine = create_engine('mysql+pymysql://root:password@localhost:5000/loykonen_matveev')
+# mysql+pymysql://root:password@localhost:5000/loykonen_matveev
+engine: Engine = create_engine('mysql+pymysql://root:password@mysql.loykonen-matveev.svc.cluster.local:3306/loykonen_matveev')
 SessionGenerator = sessionmaker(engine)
 
 metadata.create_all(bind=engine)
